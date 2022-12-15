2909ad306a212ac4ddde3bbafc754b35f60b0a79
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2022-12-15 20:37:10 -0300

    feature to create new comments :tada:

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
@@ -5,9 +7,9 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-const comments = [1, 2, 3];
-
 export function Post({ author, content, publishedAt }) {
+	const [comments, setComments] = useState([1, 2]);
+
 	const publishedDateFormatted = format(
 		publishedAt,
 		"d 'de' LLLL 'às' HH:mm'h'",
@@ -20,6 +22,14 @@ export function Post({ author, content, publishedAt }) {
 		locale: ptBR,
 		addSuffix: true,
 	});
+
+	function handleCreateNewComment() {
+		event.preventDefault();
+
+		setComments([...comments, comments.length + 1]);
+
+		console.log(comments);
+	}
 
 	return (
 		<>
@@ -56,7 +66,7 @@ export function Post({ author, content, publishedAt }) {
 					})}
 				</div>
 
-				<form className={styles.commentForm}>
+				<form onSubmit={handleCreateNewComment} className={styles.commentForm}>
 					<strong>Deixe seu feedback</strong>
 
 					<textarea placeholder='Deixe um comentário' />
